8a2ee9b44aca21f0c0e133c109db2c97b7ce7db2
Stanko998 <lazan.profil2012@gmail.com>
2021-07-31 02:24:52 +0200

    .

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,45 @@
 <template >
-  <h1>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
-    iusto architecto aliquid. Ex dicta fuga omnis optio ducimus nihil molestias
-    vel, fugit impedit soluta sit, iure cumque quam amet sapiente magnam officia
-    a accusantium aut quis. Adipisci totam voluptates obcaecati reprehenderit
-    aspernatur pariatur eos id. Perferendis non iste necessitatibus. Natus,
-    mollitia obcaecati dolorum nostrum aliquid dicta quam corporis architecto
-    quibusdam sequi quidem in quis, dolor beatae suscipit! Qui iure deserunt eum
-    neque iusto sequi magni laboriosam, eaque aperiam consequatur. Odio impedit
-    voluptatem expedita in nihil nostrum laboriosam optio modi cupiditate, quasi
-    similique voluptas deserunt perferendis eligendi. Velit iure dicta vero
-    dolores nostrum adipisci expedita sapiente nulla, eaque necessitatibus quod,
-    ducimus asperiores harum! Illo expedita nesciunt nisi tempore fugit, sequi
-    id adipisci asperiores obcaecati optio est ullam voluptas dicta cumque eius
-    accusamus vel quo assumenda corporis. Quas id porro enim maxime dolorum
-    reiciendis quo quia sit eos laborum veniam incidunt nam hic, laudantium,
-    nihil nisi in accusantium suscipit eum rerum similique explicabo
-    perspiciatis necessitatibus. Qui tempora suscipit et velit, ut modi
-    architecto? Optio quos error consequuntur, consectetur inventore deleniti
-    ipsum et vitae porro facere magni fugiat voluptatum in, repellendus magnam
-    iusto repudiandae dolorem. Veniam dicta numquam minima ipsa sapiente itaque
-    fuga!
-  </h1>
+  <div>
+    <v-toolbar>
+      <v-toolbar-title class="display-1 font-weight-thin"
+        ><span class="blue--text font-weight-black text-decoration-underline">
+          My
+        </span>
+        Project</v-toolbar-title
+      >
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <v-toolbar-item>sdas</v-toolbar-item>
+      <v-spacer></v-spacer>
+      <v-toolbar-item>ssdas</v-toolbar-item>
+      <v-spacer></v-spacer>
+      <v-toolbar-item class="mr-4">safasf</v-toolbar-item>
+    </v-toolbar>
+    <h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+      iusto architecto aliquid. Ex dicta fuga omnis optio ducimus nihil
+      molestias vel, fugit impedit soluta sit, iure cumque quam amet sapiente
+      magnam officia a accusantium aut quis. Adipisci totam voluptates obcaecati
+      reprehenderit aspernatur pariatur eos id. Perferendis non iste
+      necessitatibus. Natus, mollitia obcaecati dolorum nostrum aliquid dicta
+      quam corporis architecto quibusdam sequi quidem in quis, dolor beatae
+      suscipit! Qui iure deserunt eum neque iusto sequi magni laboriosam, eaque
+      aperiam consequatur. Odio impedit voluptatem expedita in nihil nostrum
+      laboriosam optio modi cupiditate, quasi similique voluptas deserunt
+      perferendis eligendi. Velit iure dicta vero dolores nostrum adipisci
+      expedita sapiente nulla, eaque necessitatibus quod, ducimus asperiores
+      harum! Illo expedita nesciunt nisi tempore fugit, sequi id adipisci
+      asperiores obcaecati optio est ullam voluptas dicta cumque eius accusamus
+      vel quo assumenda corporis. Quas id porro enim maxime dolorum reiciendis
+      quo quia sit eos laborum veniam incidunt nam hic, laudantium, nihil nisi
+      in accusantium suscipit eum rerum similique explicabo perspiciatis
+      necessitatibus. Qui tempora suscipit et velit, ut modi architecto? Optio
+      quos error consequuntur, consectetur inventore deleniti ipsum et vitae
+      porro facere magni fugiat voluptatum in, repellendus magnam iusto
+      repudiandae dolorem. Veniam dicta numquam minima ipsa sapiente itaque
+      fuga!
+    </h1>
+  </div>
 </template>
 
 <script>
