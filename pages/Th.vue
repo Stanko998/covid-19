2955fe57669b831be17becd21085ddbcd094cc8e
Stanko998 <lazan.profil2012@gmail.com>
2021-08-12 17:54:12 +0200

    add TypeScrpit

---
+++ pages/Th.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="black--text"></div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
